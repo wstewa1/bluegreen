0823cf20dd50672bb34e1cf7e822b974714d8fe6
wstewa1 <MrWilliamStewart@hotmail.com>
2018-08-16 14:21:58 -0500

    Update image.php

--- image.php
+++ image.php
@@ -6,9 +6,9 @@ $white = imagecolorallocate($im, 255, 255, 255);
 $deployment = getenv("COLOR");
 
 if ( $deployment == 'blue') {
-  $color = imagecolorallocate($im, 0, 0, 255);
+  $color = imagecolorallocate($im, 0, 0, 155);
 } elseif ($deployment == 'green')  {
-  $color = imagecolorallocate($im, 0, 150, 0);
+  $color = imagecolorallocate($im, 0, 155, 0);
 } else {
   $color = imagecolorallocate($im, 0, 0, 255);
 }
